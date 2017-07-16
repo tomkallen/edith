b5fe561e75a65b7f977cf17c466f89d99b54dd7f
Alexei Bykov <spbeat@mail.ru>
2017-07-16 22:03:58 +0300

    init

---
+++ editor.js
@@ -0,0 +1,48 @@
+const edith = document.getElementById("edith");
+const inputField = document.createElement("DIV");
+inputField.className = "edith-content";
+inputField.contentEditable = true;
+inputField.onkeydown = e => {
+    e.which === 9 && event.preventDefault()
+};
+const menu = document.createElement("DIV");
+menu.className = "edith-menu";
+
+const run = (command, prop = null) => {
+    document.execCommand(command, false, prop);
+    inputField.focus();
+    console.log('did smth like ' + command);
+};
+
+const buttons = [{
+        command: 'bold',
+        caption: "<b>B</b>",
+        tooltip: "Bold"
+    },
+    {
+        command: 'italic',
+        caption: "<i>I</i>",
+        tooltip: "Italic"
+    }, {
+        command: 'underline',
+        caption: "<u>U</u>",
+        tooltip: "Underline"
+    }
+];
+
+buttons.forEach(b => {
+    const button = document.createElement("DIV");
+    button.className = "edith-button";
+    button.innerHTML = b.caption;
+    button.title = b.tooltip;
+    button.onmousedown = e => {
+        e.preventDefault;
+        run(b.command)
+    }
+    menu.appendChild(button);
+});
+
+
+edith.appendChild(menu);
+edith.appendChild(inputField);
+
