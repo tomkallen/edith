21ee41919f4bcd6c38d743a6eb00d61827d37b9c
Alexei Bykov <spbeat@mail.ru>
2017-07-17 23:19:26 +0300

    up

---
+++ src/buttons.js
@@ -0,0 +1,59 @@
+export const buttons = [
+    [{
+            command: ['formatBlock', '<P>'],
+            caption: '<p>A</p>',
+            tooltip: 'Normal'
+        },
+        {
+            command: 'bold',
+            caption: '<b>B</b>',
+            tooltip: 'Bold'
+        },
+        {
+            command: 'italic',
+            caption: '<i>I</i>',
+            tooltip: 'Italic'
+        }, {
+            command: 'underline',
+            caption: '<u>U</u>',
+            tooltip: 'Underline'
+        },
+        {
+            command: 'strikeThrough',
+            caption: '<strike>S</strike>',
+            tooltip: 'Strike Through'
+        }
+    ],
+    [{
+            command: ['formatBlock', '<PRE>'],
+            caption: '<b>&lt;&nbsp;&gt;</b>',
+            tooltip: 'Code'
+        },
+        {
+            command: ['formatBlock', '<BLOCKQUOTE>'],
+            caption: '<b>&#8220;&#8221;</b>',
+            tooltip: 'Quote'
+        }
+    ],
+    [{
+            command: ['formatBlock', '<H1>'],
+            caption: '<p>H1</p>',
+            tooltip: 'H1'
+        },
+        {
+            command: ['formatBlock', '<H2>'],
+            caption: '<p>H2</p>',
+            tooltip: 'H2'
+        },
+        {
+            command: ['formatBlock', '<H3>'],
+            caption: '<p>H3</p>',
+            tooltip: 'H3'
+        },
+        {
+            command: ['formatBlock', '<H4>'],
+            caption: '<p>H4</p>',
+            tooltip: 'H4'
+        }
+    ]
+];
